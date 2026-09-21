dfc413087d9a5a021c77759cf69c3201e4a50afd
pos <pgousdal@users.noreply.github.com>
2026-09-21 23:17:41 +0200

    M0: add core smoke tests

---
+++ tests/AmiCompanion.Core.Tests/AppInfoTests.cs
@@ -0,0 +1,12 @@
+using AmiCompanion.Core;
+
+namespace AmiCompanion.Core.Tests;
+
+public sealed class AppInfoTests
+{
+    [Fact]
+    public void MilestoneIsM0() => Assert.Equal("M0", AppInfo.Milestone);
+
+    [Fact]
+    public void ProductNameIsStable() => Assert.Equal("AmiCompanion", AppInfo.Name);
+}
