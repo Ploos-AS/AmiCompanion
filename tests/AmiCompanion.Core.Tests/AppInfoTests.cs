7844767a97e10479372f325729416eace8c718a4
pos <pgousdal@users.noreply.github.com>
2026-09-22 16:36:53 +0200

    tests: advance milestone assertion to M1

--- tests/AmiCompanion.Core.Tests/AppInfoTests.cs
+++ tests/AmiCompanion.Core.Tests/AppInfoTests.cs
@@ -6,7 +6,7 @@ namespace AmiCompanion.Core.Tests;
 public sealed class AppInfoTests
 {
     [Fact]
-    public void MilestoneIsM0() => Assert.Equal("M0", AppInfo.Milestone);
+    public void MilestoneIsM1() => Assert.Equal("M1", AppInfo.Milestone);
 
     [Fact]
     public void ProductNameIsStable() => Assert.Equal("AmiCompanion", AppInfo.Name);
